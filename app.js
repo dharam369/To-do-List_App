330648505e55eaa206e58a8b70b18aa98f6868a9
dharam veer <54568191+dharam369@users.noreply.github.com>
2023-04-10 11:26:59 +0530

    This is my simple To-Do-List App
    This is my simple To-Do-List App. We have to add css also sothat it looks well.

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+
+    var today = new Date();
+    var options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    
+    res.render("list", { kindOfDay: day, newListItems: items });
+
+});
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+})
