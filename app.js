b494962c9bd4de87dbe50bc01602c710bf378256
dharam veer <54568191+dharam369@users.noreply.github.com>
2023-04-10 15:39:04 +0530

    Templating done in app.js file
    we add title templating as work.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,25 +12,37 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: 'long',
         year: 'numeric',
         month: 'long',
         day: 'numeric'
     };
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
     
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 
 });
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/Work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 })
+
+app.get("/Work", function (req,res) {
+    res.render("list", { listTitle: "Work", newListItems: workItems });
+});
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
-})
+});
