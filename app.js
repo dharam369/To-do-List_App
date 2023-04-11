5d1fa471d5c67580edae00b44bf2491852a18e5d
dharam veer <54568191+dharam369@users.noreply.github.com>
2023-04-11 07:41:02 +0530

    Requiring other js module and functions
    We create date.js file. So here we import the date.js module

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
+    const day = date.getDay();
 
-    let today = new Date();
-    let options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    };
-    let day = today.toLocaleDateString("en-US", options);
-    
     res.render("list", { listTitle: day, newListItems: items });
 
 });
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
@@ -42,10 +35,10 @@ app.get("/Work", function (req,res) {
     res.render("list", { listTitle: "Work", newListItems: workItems });
 });
 
-
 app.get("/about", function (req,res) {
     res.render("about");
-});
+})
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
